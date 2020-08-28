e929f5fbcbc2dfbfa56993fd795c310b62709f89
Jean-PierrePapin <skouo82@gmail.com>
2020-08-28 18:30:45 +0100

    Completing the email app

--- Sendingemail/Sendingemail/subscribe/views.py
+++ Sendingemail/Sendingemail/subscribe/views.py
@@ -10,7 +10,7 @@ def subscribe(request):
         subject = request.POST.get( 'subject', '' )
         message = request.POST.get( 'message', '' )
         recepient = request.POST.get( 'from email', '' )
-        send_mail(subject, message, from_email, fail_silently = false)
+        send_mail(subject, message, recepient, fail_silently = false)
         return render(request, 'subscribe/success.html', {'recepient': recepient})
     return render(request, 'subscribe/index.html', {'form': sub})
 
